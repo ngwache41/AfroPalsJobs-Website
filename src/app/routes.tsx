f4211432f98f496b4f6e4d2d58614b209faf7f50
ngwache41 <ngwache41@gmail.com>
2026-04-22 10:52:33 +0300

    fix admin login route registration

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import JobsPage from "./pages/JobsPage";
 import VisaApplicationPage from "./pages/VisaApplicationPage";
 import AdminDashboardPage from "./pages/AdminDashboardPage";
+import AdminLoginPage from "./pages/AdminLoginPage";
 import EmployerLoginPage from "./pages/EmployerLoginPage";
 import EmployerDashboardPage from "./pages/EmployerDashboardPage";
 
@@ -16,6 +17,7 @@ export const router = createBrowserRouter([
       { path: "/jobs", element: <JobsPage /> },
       { path: "/visa", element: <VisaApplicationPage /> },
       { path: "/admin", element: <AdminDashboardPage /> },
+      { path: "/admin-login", element: <AdminLoginPage /> },
       { path: "/employer-login", element: <EmployerLoginPage /> },
       { path: "/employer-dashboard", element: <EmployerDashboardPage /> },
     ],
